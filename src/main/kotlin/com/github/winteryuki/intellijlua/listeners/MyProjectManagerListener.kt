cf30e94c14988254d731b361204470eb34971df5
GitHub Action <action@github.com>
2022-04-13 15:48:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/winteryuki/intellijlua/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/winteryuki/intellijlua/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.winteryuki.intellijlua.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.winteryuki.intellijlua.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/winteryuki/intellijlua/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/winteryuki/intellijlua/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.winteryuki.intellijlua.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.winteryuki.intellijlua.MyBundle
 
 class MyApplicationService {
 
