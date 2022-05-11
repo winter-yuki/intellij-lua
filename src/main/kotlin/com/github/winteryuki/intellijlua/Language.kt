595917e76530d9aebbecc2196684f89bd0bfa25d
Andrey Stoyan <andrey.stoyan.csam@gmail.com>
2022-05-11 14:49:03 +0300

    Add icon

--- src/main/kotlin/com/github/winteryuki/intellijlua/Language.kt
+++ src/main/kotlin/com/github/winteryuki/intellijlua/Language.kt
@@ -2,6 +2,7 @@ package com.github.winteryuki.intellijlua
 
 import com.intellij.lang.Language
 import com.intellij.openapi.fileTypes.LanguageFileType
+import com.intellij.openapi.util.IconLoader
 import javax.swing.Icon
 
 object LuaLanguage : Language("Lua")
@@ -10,5 +11,5 @@ object LuaFileType : LanguageFileType(LuaLanguage) {
     override fun getName(): String = "Lua"
     override fun getDescription(): String = "Lua"
     override fun getDefaultExtension(): String = "lua"
-    override fun getIcon(): Icon? = null
+    override fun getIcon(): Icon = IconLoader.getIcon("icons/logo.png", LuaFileType::class.java)
 }
