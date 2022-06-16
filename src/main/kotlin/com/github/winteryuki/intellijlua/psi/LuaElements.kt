ccc1e5a1b93b186f551f8c36764e7438d81083e8
Andrey Stoyan <andrey.stoyan.csam@gmail.com>
2022-06-16 12:55:36 +0300

    Add simple resolve

--- src/main/kotlin/com/github/winteryuki/intellijlua/psi/LuaElements.kt
+++ src/main/kotlin/com/github/winteryuki/intellijlua/psi/LuaElements.kt
@@ -7,6 +7,11 @@ import com.intellij.extapi.psi.PsiFileBase
 import com.intellij.lang.ASTNode
 import com.intellij.openapi.fileTypes.FileType
 import com.intellij.psi.FileViewProvider
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiNamedElement
+import com.intellij.psi.PsiReference
+import com.intellij.psi.PsiReferenceBase
+import com.intellij.psi.util.descendants
 
 class LuaFile(viewProvider: FileViewProvider) : PsiFileBase(viewProvider, LuaLanguage) {
     override fun getFileType(): FileType = LuaFileType
@@ -16,6 +21,8 @@ class LuaFile(viewProvider: FileViewProvider) : PsiFileBase(viewProvider, LuaLan
 abstract class LuaPsiElement(node: ASTNode) : ASTWrapperPsiElement(node) {
     override fun getContainingFile(): LuaFile? = super.getContainingFile() as? LuaFile
 }
+
+abstract class LuaNamedPsiElement(node: ASTNode) : LuaPsiElement(node), PsiNamedElement
 
 class LuaBlockElement(node: ASTNode) : LuaPsiElement(node)
 
@@ -64,3 +71,32 @@ class LuaFieldListElement(node: ASTNode) : LuaPsiElement(node)
 class LuaTableConstructorElement(node: ASTNode) : LuaPsiElement(node)
 
 class LuaBinOpElement(node: ASTNode) : LuaPsiElement(node)
+
+class LuaNameDeclarationElement(node: ASTNode) : LuaNamedPsiElement(node) {
+    override fun setName(name: String): PsiElement = this
+
+    override fun getName(): String = node.text
+}
+
+class LuaNameReferenceElement(node: ASTNode) : LuaPsiElement(node) {
+    override fun getReference(): PsiReference? {
+        val targetName = node.text
+        var scope: PsiElement? = this.parent
+        while (scope != null) {
+            val target = scope.children
+                .flatMap {
+                    it
+                        .descendants { it !is LuaBlockElement }
+                        .filterIsInstance<LuaNameDeclarationElement>()
+                }
+                .find { it.name == targetName }
+            if (target != null) {
+                return object : PsiReferenceBase<PsiElement>(this, this.textRangeInParent) {
+                    override fun resolve(): PsiElement = target
+                }
+            }
+            scope = scope.parent
+        }
+        return null
+    }
+}
